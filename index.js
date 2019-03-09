b3a067cccc58603d3a71b01c6e3b36ca1c6e3b0d
Ethan Smith <ethanfsmith@gmail.com>
2019-03-08 22:30:15 -0800

    js cleanup

--- index.js
+++ index.js
@@ -26,35 +26,17 @@ $(document).ready(function(){
       });
     });
 
-    var navTrigger = document.getElementsByClassName('nav-trigger')[0],
-    body = document.getElementsByTagName('body')[0],
-    linkTrigger = document.querySelectorAll('ul.nav a, .overlay');
 
-    navTrigger.addEventListener('click', toggleNavigation);
-
-    linkTrigger.forEach(
-      function(link){
-        link.addEventListener('click', hideNavigation);
-      });
-
-    function toggleNavigation(event) {
+    $('.nav-trigger').click(function() {
       event.preventDefault();
-      body.classList.toggle('nav-open');
-    }
+      $('body').toggleClass('nav-open');
+    })
 
-    function hideNavigation(event) {
-      setTimeout(function(){body.classList.remove('nav-open')},500);
-    }
-
-    /*
-    window.onscroll = function() {
-      if (document.body.scrollTop > 200) {
-        body.classList.add('scrolled');
-      } else {
-        body.classList.remove('scrolled');
-      }
-    }
-    */
+    $('ul.nav a, .overlay').click(function() {
+      setTimeout(function() {
+        $('body').removeClass('nav-open');
+      }, 400);
+    })
 
 
   // var $contactForm = $('#contact-form');
