4f6c2caba56c2faabbd8513f2fcf27af55652f58
Ethan Smith <ethanfsmith@gmail.com>
2019-03-08 22:41:11 -0800

    cleanup

--- index.js
+++ index.js
@@ -1,8 +1,5 @@
 $(document).ready(function(){
 
-
-  // DESKTOP-SPECIFIC JS
-
     // handler for marketing services nav list
     $('.navigation li:first-child').hover(function(){
       $('#services').show();
@@ -37,36 +34,6 @@ $(document).ready(function(){
         $('body').removeClass('nav-open');
       }, 400);
     })
-
-
-  // var $contactForm = $('#contact-form');
-  // $contactForm.submit(function(e) {
-  // 	e.preventDefault();
-  //   $('#message-container').html('');
-  //
-  // 	$.ajax({
-  // 		url: 'https://formspree.io/info@10zingdigital.com',
-  // 		method: 'POST',
-  //     data: {
-  //       Name: $('#contact-form').find("input[name='name']").val(),
-  //       Email: $('#contact-form').find("input[name='_replyto']").val(),
-  //       Company: $('#contact-form').find("input[name='company']").val(),
-  //       Phone: $('#contact-form').find("input[name='phone']").val(),
-  //       Message: $('#contact-form').find("textarea[name='message']").val()
-  //     },
-  //     dataType: 'json',
-  //     success: function(data) {
-  //       $('#contact-form input').not('.submit').val('');
-  //       $('#contact-form textarea').val('');
-  //       console.log('success ', data);
-  //       $('#message-container').append('<span class="res">Your message has been sent. Thanks for contacting us!</span>');
-  // 		},
-  // 		error: function(err) {
-  //       console.log('error ', err);
-  //       $('#message-container').append('<span class="res">Looks like there was an error. Please try again.</span>');
-  //     }
-  // 	});
-  // });
 
 });
 
