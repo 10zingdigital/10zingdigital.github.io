63b9768dbc24b3e5b81f8e9dac6dd10b1e2e52d2
Ethan Smith <ethanfsmith@gmail.com>
2019-08-05 22:51:02 -0700

    update refinements

--- index.js
+++ index.js
@@ -1,67 +1,61 @@
-$(document).ready(function(){
+$(document).ready(function() {
+  // handler for marketing services nav list
+  $(".navigation li:first-child").hover(function() {
+    $("#services").show();
+    $(this)
+      .children("div")
+      .addClass("hover-over-services");
+    $(this)
+      .find("ul")
+      .addClass("hover-over-services-ul");
 
-    // handler for marketing services nav list
-    $('.navigation li:first-child').hover(function(){
-      $('#services').show();
-      $(this).children('div').addClass('hover-over-services');
-      $(this).find('ul').addClass('hover-over-services-ul');
-
-      // marketing services sub-nav
-      $('#services li').hover(function(){
-        $(this).css('color', '#FF8110');
-        $(this).children('div').addClass('underlined');
-        $(this).mouseleave(function(){
-          $(this).css('color', '#fff');
-          $(this).children('div').removeClass('underlined');
-        });
-      });
-
-      $(this).mouseleave(function(){
-        $('#services').hide();
-        $(this).children('div').removeClass('hover-over-services');
-        $(this).find('ul').removeClass('hover-over-services-ul');
+    // marketing services sub-nav
+    $("#services li").hover(function() {
+      $(this).css("color", "#FF8110");
+      $(this)
+        .children("div")
+        .addClass("underlined");
+      $(this).mouseleave(function() {
+        $(this).css("color", "#fff");
+        $(this)
+          .children("div")
+          .removeClass("underlined");
       });
     });
 
-
-    $('.nav-trigger').click(function() {
-      event.preventDefault();
-      $('body').toggleClass('nav-open');
-    })
-
-    $('ul.nav a, .overlay').click(function(e) {
-
-      // smooth scroll to target
-      e.preventDefault();
-      $('body.home').animate(
-        {
-          scrollTop: $($(this).attr('href')).offset().top,
-        },
-        500,
-        'linear'
-      );
-
-      // hide the mobile menu on click
-      setTimeout(function() {
-        $('body').removeClass('nav-open');
-      }, 400);
-    })
-
-/*
-    // https://css-tricks.com/the-trick-to-viewport-units-on-mobile/
-    // First we get the viewport height and we multiple it by 1% to get a value for a vh unit
-    let vh = window.innerWidth * 0.01;
-    // Then we set the value in the --vh custom property to the root of the document
-    document.documentElement.style.setProperty('--vh', `${vh}px`);
-
-    // We listen to the resize event
-    window.addEventListener('resize', () => {
-      // We execute the same script as before
-      let vh = window.innerWidth * 0.01;
-      document.documentElement.style.setProperty('--vh', `${vh}px`);
+    $(this).mouseleave(function() {
+      $("#services").hide();
+      $(this)
+        .children("div")
+        .removeClass("hover-over-services");
+      $(this)
+        .find("ul")
+        .removeClass("hover-over-services-ul");
     });
-    */
-
   });
 
+  $(".nav-trigger").click(function() {
+    event.preventDefault();
+    $("body").toggleClass("nav-open");
+  });
 
+  $("ul.nav a, .overlay").click(function() {
+    // hide the mobile menu on click
+    setTimeout(function() {
+      $("body").removeClass("nav-open");
+    }, 400);
+  });
+
+  // smooth scroll to target on home page
+
+  $("body.home ul.nav ul a").click(function(e) {
+      e.preventDefault();
+      $("html,body").animate(
+        {
+          scrollTop: $($(this).attr("href")).offset().top
+        },
+        500,
+        "linear"
+      );
+  });
+});
